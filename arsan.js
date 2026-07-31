b6b8204f7b06a9099a6f397f6abb23a23e091db5
Arsan Acharya <arsanacharya063@gmail.com>
2026-07-31 22:07:28 +0545

    various thing in the js basic

--- arsan.js
+++ arsan.js
@@ -1,16 +1,85 @@
-let a = 10;
-let b = 3;
-console.log(a + b); 
-console.log(a-b);
+// let a = 10;
+// let b = 3;
+// console.log(a + b); 
+// console.log(a-b);
 
 
 
 
-console.log("sum of 10 and 20 is " + a + b);//this will be concated 
+// console.log("sum of 10 and 20 is " + a + b);//this will be concated 
 
 
-console.log(a+b+"sum of 10 and 20");
+// console.log(a+b+"sum of 10 and 20");
 
 
-console.log("1"-1);
+// console.log("1"-1);
 
+// let age = prompt("enter your age");
+// age = Number(age);
+// console.log(age);
+
+//type castin is the processs of changingh the type of the variables data type like number to string or string to number etc
+
+
+//swapping  there are three thing we will learn today 
+//1. with a extra veriable
+//  let a = 10;
+// let b = 20;
+// let c = a;
+// c = a;
+// a = b;
+// b = c;
+// console.log(a);
+// console.log(b);
+
+//now without the extra virable
+
+//   let a = 10;
+// let b = 20;
+// a = a+b; // now a = 30 
+// b = a-b; // now b = 30-20 which is 10
+// a = a-b; // now a = 30-10 which is 20 
+// console.log(a);
+// console.log(b);
+// now there is 3rd 
+
+// let a = 20;
+// let b = 10;
+
+// [a,b]=[b,a]
+// console.log(a,b); // this is called destructuring assignment in js
+
+// conditional statement in js
+
+// can  you vote 
+
+// age = Number(prompt("what is your age?"));
+
+// if (isNaN(age)) {
+//     console.log("Please enter a valid age.");
+// } else if (age >= 18) {
+//     console.log("You can vote.");
+// } else {
+//      console.log("you cannot vote");
+
+// }
+
+
+// //loops
+// let n = Number(prompt("Enter the value of n"));
+
+// if (isNaN(n)) {
+//     console.log("Enter a valid number");
+// } else {
+//     if (n > 0) {
+//         let sum = 0;
+
+//         for (let i = 1; i <= n; i++) {
+//             sum = sum + i;
+//         }
+
+//         console.log("The sum of the numbers is " + sum);
+//     } else {
+//         console.log("Please enter a positive number");
+//     }
+// }
