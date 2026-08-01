639b818462026f89d3e7265b9d8191bb74a041bb
Arsan Acharya <arsanacharya063@gmail.com>
2026-08-01 21:40:35 +0545

    some aray stuff

--- arsan.js
+++ arsan.js
@@ -82,4 +82,11 @@
 //     } else {
 //         console.log("Please enter a positive number");
 //     }
+// // }
+
+// //today we will rearn about array in js
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
