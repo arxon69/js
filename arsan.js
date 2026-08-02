74c25d79678a9c8db15f20ddb34f325277bae8fc
Arsan Acharya <arsanacharya063@gmail.com>
2026-08-02 22:06:49 +0545

    to add all the element of the arrray in the sum veriable

--- arsan.js
+++ arsan.js
@@ -148,18 +148,26 @@
 
 // lets do another one for the day which is to find the second smallest number in the array without using the built in methods
 
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let min = arr[0];
+// let secondMin = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         secondMin = min;
+//         min = arr[i];
+//     }
+//     else if (arr[i] < secondMin && arr[i] != min) {
+//         secondMin = arr[i];
+//     }
+// }
+// console.log("The second smallest number is " + secondMin);  
+
+
+//one more dsa problem is to find the sum of all the elements in the array without using the built in methods
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let min = arr[0];
-let secondMin = arr[0];
-for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-        secondMin = min;
-        min = arr[i];
-    }
-    else if (arr[i] < secondMin && arr[i] != min) {
-        secondMin = arr[i];
-    }
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
 }
-console.log("The second smallest number is " + secondMin);  
-
-
+console.log("The sum of all the elements in the array is " + sum);
