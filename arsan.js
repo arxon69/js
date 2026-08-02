8337701dcc6a2d74bb8e679a474ad71d4d1ad4d4
Arsan Acharya <arsanacharya063@gmail.com>
2026-08-02 22:03:10 +0545

    i have done another dsa

--- arsan.js
+++ arsan.js
@@ -128,3 +128,21 @@
 // console.log("The maximum number is " + max);
 // console.log("The minimum number is " + min);
 
+
+
+//lets do the anothr toady lets do another dsa problem in js which is to find the second largest number in the array without using the built in methods
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let max = arr[0];
+let secondMax = arr[0];
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        secondMax = max;
+        max = arr[i];
+    }
+    else if (arr[i] > secondMax && arr[i] != max) {
+        secondMax = arr[i];
+    }
+}
+console.log("The second largest number is " + secondMax);
+
